fc07756b7d33ea707900f9fb802ee55ae1c55160
Shridhan <shridhan.suman2023@vitstudent.ac.in>
2025-12-30 00:48:56 +0530

    initialized frontend

---
+++ client/src/data/features.tsx
@@ -0,0 +1,20 @@
+import type { IFeature } from "../types";
+
+export const featuresData: IFeature[] = [
+    {
+        icon: "/assets/zap-icon.svg",
+        title: "Smart Analysis",
+        description: "Launch production-ready pages in minutes with prebuilt components.",
+    },
+    {
+        icon: "/assets/thumb-icon.svg",
+        title: "Eye catching design",
+        description: "Launch production-ready pages in minutes with prebuilt components.",
+    },
+    {
+        icon: "/assets/shape-icon.svg",
+        title: "Fully Editable",
+        description: "Launch production-ready pages in minutes with prebuilt components.",
+    },
+     
+];
